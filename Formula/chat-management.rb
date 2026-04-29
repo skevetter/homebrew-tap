9c4555e65fa518e8de3543127cbd28539233cd2b
Samuel K <skevetter@pm.me>
2026-04-29 17:34:01 -0500

    chat-management 0.3.1

--- Formula/chat-management.rb
+++ Formula/chat-management.rb
@@ -1,9 +1,9 @@
 class ChatManagement < Formula
   desc "CLI communication management tool with SQLite backend"
   homepage "https://github.com/skevetter/chat-management"
-  url "https://github.com/skevetter/chat-management/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "0a6cbe5676c0e8141706948f2664df1d4d456ffc51fb35482d973aa06d8ee4f7"
-  version "0.3.0"
+  url "https://github.com/skevetter/chat-management/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "acdb7d7de4e9ee871360c5afc78570ca6f719f34d49e97100151a60c34432944"
+  version "0.3.1"
   license "MIT"
 
   depends_on "rust" => :build
