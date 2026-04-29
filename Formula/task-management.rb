67608329d76cce3f02c4cb93c60b60ab6585a87b
Samuel K <skevetter@pm.me>
2026-04-28 23:04:40 -0500

    chore: update task-management formula to v0.2.1

--- Formula/task-management.rb
+++ Formula/task-management.rb
@@ -1,9 +1,9 @@
 class TaskManagement < Formula
   desc "CLI task management tool with SQLite backend"
   homepage "https://github.com/skevetter/task-management"
-  url "https://github.com/skevetter/task-management/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "4e3bd812fcd42e156c15ded84f8fff277a65a7eaa62ddf56490c6625fa31be49"
-  version "0.2.0"
+  url "https://github.com/skevetter/task-management/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "04f82553009671a68b3f7191230368716f8e54858630dc7ee25b25c06ea503e6"
+  version "0.2.1"
   license "MIT"
 
   depends_on "rust" => :build
