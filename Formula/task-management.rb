2be74eef2f65abd1e3394b5602949836b8371083
Samuel K <skevetter@pm.me>
2026-04-29 08:54:13 -0500

    task-management 0.4.0: namespace scoping + list pagination

--- Formula/task-management.rb
+++ Formula/task-management.rb
@@ -1,9 +1,9 @@
 class TaskManagement < Formula
   desc "CLI task management tool with SQLite backend"
   homepage "https://github.com/skevetter/task-management"
-  url "https://github.com/skevetter/task-management/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "f7c4f2335f81d37b8b8e27a4e01d151b6357220ee22eb88ce22a89bc5c95e8c7"
-  version "0.3.0"
+  url "https://github.com/skevetter/task-management/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "4e9f5acb251e0366851808bd85e59d5a03d5c04cdc904ec000b0d6c38f87b350"
+  version "0.4.0"
   license "MIT"
 
   depends_on "rust" => :build
