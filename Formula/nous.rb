b0bb18055fb89bef3e8d24a3709f5c342ad93472
Samuel K <skevetter@pm.me>
2026-04-30 10:09:42 -0500

    feat: add nous formula for v0.1.0

---
+++ Formula/nous.rb
@@ -0,0 +1,16 @@
+class Nous < Formula
+  desc "The nous platform CLI"
+  homepage "https://github.com/skevetter/nous"
+  url "https://github.com/skevetter/nous.git", tag: "v0.1.0", using: :git
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--path", "crates/nous-cli", "--root", prefix
+  end
+
+  test do
+    assert_match "nous", shell_output("#{bin}/nous --help")
+  end
+end
